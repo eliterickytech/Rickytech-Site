19ed001195c5d75bc24fbd5f3555c034b8c21d34
Ricardo Macedo Perdigão <57601338+eliterickytech@users.noreply.github.com>
2026-06-02 02:43:50 -0300

    Delete src directory

--- src/components/layout/Footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <span>&copy; {new Date().getFullYear()} Rickytech &mdash; Finance Control v1.0</span>
-    </footer>
-  );
-}
